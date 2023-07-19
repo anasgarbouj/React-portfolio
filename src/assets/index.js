433ac42896059719bc074bdd73a7ca8f5dc221fa
anas garbouj <anaskping@gmail.com>
2023-07-19 22:48:04 +0100

    Add SPA redirect rule

--- src/assets/index.js
+++ src/assets/index.js
@@ -20,7 +20,7 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-import Symfony from "./tech/Symfony.png";
+import Symfony from "./tech/symfony.png";
 
 
 import meta from "./company/meta.png";
